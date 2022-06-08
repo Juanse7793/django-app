e35cb272fc7e7966a4bf0a6b7363318e8b6d4892
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-08 17:09:29 -0500

    templates with Django

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'hello.html', {'name': 'Juancho'})
