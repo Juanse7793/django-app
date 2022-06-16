a4872b899368777c18f3e22734f99ffb4b1b04d7
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-16 12:04:52 -0500

    choises

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -9,15 +9,31 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
     MEMBERSHIP_CHIOCES = [
-        ('B', 'Bronze'),
-        ('S', 'Silver'),
-        ('G', 'Gold'),
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
     ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1 ,choices=MEMBERSHIP_CHIOCES)
+    membership = models.CharField(max_length=1 ,choices=MEMBERSHIP_CHIOCES, default=MEMBERSHIP_BRONZE)
 
+class Orders(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
