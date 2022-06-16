492ceea63dcceebdb72a9bd4bb41db851d26a576
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-16 11:59:21 -0500

    models

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    MEMBERSHIP_CHIOCES = [
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1 ,choices=MEMBERSHIP_CHIOCES)
+
