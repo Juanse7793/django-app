34b3dd259281fd234495d9a9a10385b6c943ffa2
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-16 14:42:00 -0500

    many to many field in product-promotions

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -2,6 +2,10 @@ from itertools import product
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
@@ -12,7 +16,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
