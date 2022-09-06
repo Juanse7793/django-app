367790da711020b0aa7b58c0d7b6a807d1c0f870
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-09-06 17:47:05 -0500

    products_count in collection

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from . import models
 # Register your models here.
 
@@ -22,5 +23,15 @@ class ProductAdmin(admin.ModelAdmin):
         return f'OK ({product.inventory})'
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
 
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
