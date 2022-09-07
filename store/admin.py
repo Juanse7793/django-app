e2f57c2f8cd813ef957731856cea68b7490c6f9a
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-09-06 19:16:12 -0500

    search bar

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html
 from . import models
 # Register your models here.
 
@@ -7,12 +9,14 @@ from . import models
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable: str = ['membership']
+    search_fields: str = ['first_name__istartswith', 'last_name__istartswith']
     list_per_page: int = 10
 
+    
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
     list_per_page: int = 10
 
@@ -29,7 +33,11 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + f'collection__id__exact={collection.id}')
+        return format_html('<a href="{}">{}</a>',url ,collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
